01c06a3d34a6d42e8acafc8ee9ffd8141539d009
calebpenning <calebpenning@mail.com>
2021-08-13 14:48:24 -0700

    queue and stack implementations in js with some methods

---
+++ stack.js
@@ -0,0 +1,62 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to "top" of the stack. Returns undefined. */
+
+  push(val) {
+    let newNode = new Node(val)
+    if (!this.first) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      let temp = this.first
+      this.first = newNode
+      this.first.next = temp
+    }
+
+    this.size++
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if (this.size === 0) throw new Error("Can't pop empty stack")
+    let toRemove = this.first
+    let retVal = toRemove.val
+    let toReplace = toRemove.next
+    this.first = toReplace
+    this.size--
+    return retVal
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+    return this.first.val
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return this.size > 0 ? false : true
+  }
+}
+
+module.exports = Stack;
